c701f4a5cb6e47ccd2cda17094d2bb2dad2bee62
Siddharthmainahihu <B22135@students.iitmandi.ac.in>
2024-04-27 12:51:18 +0530

    Added NavBar and some minor updates

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar.js";
+import ImageView from "./components/ImageView.js";
+import ImageUpload from "./components/ImageUpload.js";
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+    <Navbar />
+      <Routes>
+      <Route index element={<ImageUpload />} />
+      <Route path="/home" element={<ImageUpload />} />
+      <Route path="/display" element={<ImageView />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ src/components/Navbar.js
@@ -0,0 +1,17 @@
+// Navbar.js
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Navbar = () => {
+    return (
+        <nav>
+            <ul>
+                <li>
+                    <Link to="/">Home</Link>
+                </li>
+            </ul>
+        </nav>
+    );
+};
+
+export default Navbar;
