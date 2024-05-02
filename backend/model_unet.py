d5061c41d4a038b62301c2b44c461a08a9b1cd82
07Sarthak <120120194+07Sarthak@users.noreply.github.com>
2024-05-03 03:46:35 +0530

    Update test_file

--- backend/model_unet.py
+++ backend/model_unet.py
@@ -8,8 +8,8 @@ model.load_weights(model_weight_link)
 
 test_path="path to the test data folder"
 files=os.listdir(test_path+'/images')
-images_no=len(files)
 files.sort()
+images_no=len(files)
 testGene = testGenerator(test_path+'/images',as_gray=False)
 begin=time.time()
 results = model.predict_generator(testGene,images_no,verbose=1)
