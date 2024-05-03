08f9e32997c3e05fe206c1ae97bc77c7b2202690
Siddharth Shainesh <120120270+Siddharthmainahihu@users.noreply.github.com>
2024-05-03 11:13:56 +0530

    Update ImageView.js

--- src/components/ImageView.js
+++ src/components/ImageView.js
@@ -1,9 +1,19 @@
 import React from 'react';
+import Image from '../0_predict.png';
 
 const ImageView = ({ imageUrl }) => {
   return (
-    <div>
-      <img src={imageUrl} alt="Processed Image" />
+    <div className="container mt-5">
+      <div className="row justify-content-center">
+        <div className="col-md-8">
+          <div className="card">
+            <div className="card-body">
+              <h5 className="card-title">Processed Image</h5>
+              <img src={Image} className="img-fluid" alt="Processed Image" />
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
