23ef8d7b2b78661f091514231fdcf6f58eb59d03
Siddharth Shainesh <120120270+Siddharthmainahihu@users.noreply.github.com>
2024-05-03 11:13:30 +0530

    Update ImageUpload.js

--- src/components/ImageUpload.js
+++ src/components/ImageUpload.js
@@ -1,26 +1,36 @@
 import React, { useState } from 'react';
+import './styles.css';
 
 const ImageUpload = () => {
   const [file, setFile] = useState(null);
+  const [isProcessing, setIsProcessing] = useState(false);
 
   const handleFileChange = (e) => {
     setFile(e.target.files[0]);
   };
 
   const handleSubmit = async () => {
+    setIsProcessing(true);
     const formData = new FormData();
     formData.append('file', file);
     
     try {
-      const response = await fetch('http://localhost:5000/process-image', {
-        method: 'POST',
-        body: formData
-      });
-      const data = await response.json();
-      // Redirect to the ImageView page passing the image URL as a parameter
-      window.location.href = `/image-view?imageUrl=${data.imageUrl}`;
+        const response = await fetch('http://localhost:5000/process-image', {
+            method: 'POST',
+            body: formData
+        });
+        if (!response.ok) {
+            throw new Error('Failed to process image');
+        }
+        const data = await response.json();
+        // Redirect to the ImageView page passing the image URL as a parameter
+        console.log('Received URL:', data.imageUrl);
+        window.location.href = `/display`;
     } catch (error) {
-      console.error('Error:', error);
+        console.error('Error:', error);
+        // Handle error (e.g., display error message to user)
+    } finally {
+        setIsProcessing(false);
     }
   };
 
@@ -30,9 +40,18 @@ const ImageUpload = () => {
         <div className="col-md-6">
           <div className="card bg-light">
             <div className="card-body">
-              <h5 className="card-title">Upload Image</h5>
-              <input type="file" className="form-control mb-3" accept="image/*" onChange={handleFileChange} />
-              <button onClick={handleSubmit} className="btn btn-primary">Process Image</button>
+              {isProcessing ? (
+                <div className="text-center">
+                  <p>Please wait while we process your image...</p>
+                  {/* You can add a loading spinner or animation here */}
+                </div>
+              ) : (
+                <>
+                  <h5 className="card-title">Upload Image</h5>
+                  <input type="file" className="form-control mb-3" accept="image/*" onChange={handleFileChange} />
+                  <button onClick={handleSubmit} className="btn btn-primary">Process Image</button>
+                </>
+              )}
             </div>
           </div>
         </div>
